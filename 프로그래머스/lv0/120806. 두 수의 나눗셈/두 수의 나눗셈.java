ceab9bf155fdb6d4450b877a0f975a5122829b1c
Cheon-Byeong-Chan <76854159+Cheon-Byeong-Chan@users.noreply.github.com>
2023-07-24 22:15:45 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,8 +1,14 @@
+// class Solution {
+//     public int solution(int num1, int num2) {
+//         double answer = 0;
+//         answer = (double)num1 / num2 * 1000;
+//         int result = (int)answer;
+//         return result;
+//     }
+// }
 class Solution {
     public int solution(int num1, int num2) {
-        double answer = 0;
-        answer = (double)num1 / num2 * 1000;
-        int result = (int)answer;
-        return result;
+        double result = (double) num1 / (double) num2;
+        return (int) (result * 1000);
     }
 }
